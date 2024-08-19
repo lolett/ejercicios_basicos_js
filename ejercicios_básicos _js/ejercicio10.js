eb1fecf25fe6cbc49c1124e1c33b96b51e3b878c
mapaca_rules <105522791+lolett@users.noreply.github.com>
2024-08-19 23:54:04 +0200

    Update ejercicio10.js

--- ejercicios_básicos _js/ejercicio10.js
+++ ejercicios_básicos _js/ejercicio10.js
@@ -5,6 +5,7 @@ Puedes usar este array para probarla: */
 const numbers = [12, 21, 38, 5, 45, 37, 6]
 
 let total = 0
+
 function average(numberList) {
   for (let i = 0; i < numberList.length; i++) {
     const number = numberList[i]
